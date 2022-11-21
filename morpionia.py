deccb20e56bdbe00e383e25244146604a69d2960
yoannklt <ykerlogot@gaming.tech>
2022-11-21 14:38:38 +0100

    ia fix

--- morpionia.py
+++ morpionia.py
@@ -2,7 +2,11 @@ from random import randint
 caseJouable = True
 joueur = input("Quel est votre pseudo ?: \n")
 coupJoueur = "X"
+coupMilieuJoueur = False
+coupMilieuIA = False
 tableJeu = [["-","-","-"],["-","-","-"],["-","-","-"]]
+gameRound = 0 
+
 
 def showTable(tableJeu):
     for row in tableJeu :
@@ -19,7 +23,7 @@ def caseRemplie(tableJeu, posx, posy):
         return True
 
 #######################
-def is_player_win(board, player):
+def isPlayerWin(board, player):
     win = None
 
     n = len(board)
@@ -62,7 +66,7 @@ def is_player_win(board, player):
         return win
     return False
 
-def is_board_filled(board):
+def isBoardFilled(board):
     for row in board:
         for item in row:
             if item == '-':
@@ -70,53 +74,67 @@ def is_board_filled(board):
     return True
 #######################
 
-
-
+def coupIA(tableau, x, y):
+    tableau[x][y] = 'O'
 
 
 
 def ia(tableau):
+    global gameRound, coupMilieuIA, coupMilieuJoueur
     if tableau[1][1] == '-' : 
+        coupMilieuIA = True
         tableau[1][1] = 'O'
-    elif tableau[1][1] != '-' :
+    elif tableau[1][1] != '-' and tableau[1][1] == coupJoueur :
+        coupMilieuJoueur = True
         tableau[0][0] = 'O'
+    elif tableau[2][2] == 'X' :
+        tableau[1][0] = coupIA(tableau, 1,0)
+    elif bonCourage(tableJeu, 'O') == False:
+        if bonCourage(tableJeu, 'X') == False:
+            if tableau[1][0] == '-' :
+                coupIA(tableau, 1, 0)
+            else :
+                coupIA(tableau,1,2)
+            
+   
+    print(bonCourage(tableJeu, 'O'))
+    print(bonCourage(tableJeu, 'X'))
 
 
-
-def isIaWin(case1, case2, case3) :
-    if case1 == case2 == 'O' and case3 == '-' :
+def isIaWin(case1, case2, case3, coupDuJoueur) :
+    if case1 == case2 == coupDuJoueur and case3 == '-' :
         return True, 2
-    elif case1 == case3 == 'O' and case2 == '-' :
+    elif case1 == case3 == coupDuJoueur and case2 == '-' :
         return True, 1
-    elif case2 == case3 == 'O' and case1 == '-' :
+    elif case2 == case3 == coupDuJoueur and case1 == '-' :
         return True, 0
+    return False
 
-    if case1 == case2 == coupJoueur and case3 == '-' :
-        return 2
-    elif case1 == case3 == coupJoueur and case2 == '-' :
-        return 1
-    elif case2 == case3 == coupJoueur and case1 == '-' :
-        return 0
-
-def bonCourage(tableau):
+def bonCourage(tableau, coupDuJoueur):
     for i in range(3) :
-        ligne = isIaWin(tableau[i][0], tableau[i][1], tableau[i][2])
-        colonne = isIaWin(tableau[i][0], tableau[i][1], tableau[i][2])
-        if ligne == True :
-            tableau[i][ligne] = 'O'
-            return tableau
-        if isIaWin(tableau[0][i], tableau[1][i], tableau[2][i]) == True :
-            tableau[colonne][i] = 'O'
-            return tableau 
-    diagonale1 = isIaWin(tableau[0][0], tableau[1][1], tableau[2][2])
-    diagonale2 = isIaWin(tableau[0][2], tableau[1][1], tableau[2][0])
-    if diagonale1 == True :
-        tableau[diagonale1][diagonale1] = 'O'
+        ligne = isIaWin(tableau[i][0], tableau[i][1], tableau[i][2], coupDuJoueur)
+        colonne = isIaWin(tableau[0][i], tableau[1][i], tableau[2][i], coupDuJoueur)
+        if ligne != False :
+            tableau[i][ligne[1]] = 'O'
+            #print(ligne)
+            return True
+        if colonne!= False :
+            tableau[colonne[1]][i] = 'O'
+            #print(colonne)
+            return True 
+    diagonale1 = isIaWin(tableau[0][0], tableau[1][1], tableau[2][2], coupDuJoueur)
+    diagonale2 = isIaWin(tableau[0][2], tableau[1][1], tableau[2][0], coupDuJoueur)
+    #print(diagonale1)
+    #print(diagonale2)
+    if diagonale1!= False :
+        tableau[diagonale1[1]][diagonale1[1]] = 'O'
         return tableau
-    if diagonale2 == True :
-        tableau[diagonale2][2 - diagonale2] = 'O'
+    if diagonale2!= False :
+        tableau[diagonale2[1]][2 - diagonale2[1]] = 'O'
         return tableau
     
+    return False
+
 def game():
     global joueur, coupJoueur, tableJeu
     coupCorrect = False
@@ -128,16 +146,19 @@ def game():
             if caseRemplie(tableJeu, choiceX, choiceY) != True :
                 tableJeu[choiceX][choiceY] = coupJoueur
                 coupCorrect = True
-            bonCourage(tableJeu)
-            ia(tableJeu)
+        ia(tableJeu)
         showTable(tableJeu)
         
-        if is_player_win(tableJeu, coupJoueur):
+        if isPlayerWin(tableJeu, 'O'):
+            print("L'IA à gagné + ratio")
+            joueurGagnant = True
+            break
+        elif isPlayerWin(tableJeu, coupJoueur):
             print("Le joueur " + joueur + " a gagné ! :)")
             joueurGagnant = True
             break
 
-        if is_board_filled(tableJeu):
+        if isBoardFilled(tableJeu):
             print("Egalité !")
             break
 
